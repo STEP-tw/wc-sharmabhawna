512ccd2363e0097b91be16eeb2b854a31c9745c8
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-27 14:26:03 +0530

    Modified format
    Renmed variables and removed unnecessary initial value for reducer which
    was array of zeroes

--- src/formatter.js
+++ src/formatter.js
@@ -9,17 +9,18 @@ const format = function (countsDetails) {
     if (countsDetails.length == 1) {
         return singleFileFormater(countsDetails[0]);
     }
-    let result = countsDetails.map(singleFileFormater);
-    let totalCounts = countsDetails.reduce(totalGenerator, [0, 0, 0]).concat("total").join(SPACE);
-    return result = result.concat(totalCounts).join(NEWLINE);
+    let requiredOutput = countsDetails.map(singleFileFormater);
+    let totalCounts = countsDetails.reduce(totalGenerator).counts;
+    let total = totalCounts.concat("total").join(SPACE);
+    return requiredOutput = requiredOutput.concat(total).join(NEWLINE);
 };
 
 const totalGenerator = function (accumulator, { counts }) {
     let total = [];
     for (let index = 0; index < counts.length; index++) {
-        total = total.concat(accumulator[index] + counts[index])
+        total = total.concat(accumulator.counts[index] + counts[index])
     }
-    return total;
+    return total.counts = total;
 };
 
 module.exports = { format };
