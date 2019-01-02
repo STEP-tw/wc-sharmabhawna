ce31ed5e5c3c42b5a4c937201702b2e7ee4844e3
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-01-02 14:21:32 +0530

    Renamed argument readFileSync to reader

--- src/lib.js
+++ src/lib.js
@@ -1,39 +1,39 @@
 const { splitByNewLine, splitBySpace } = require("./util/string.js");
 
-const countLines = function (data) {
-    return splitByNewLine(data).length - 1;
+const countLines = function(data) {
+  return splitByNewLine(data).length - 1;
 };
 
-const countWords = function (data) {
-    const wordCounter = function (count, line) {
-        return count + splitBySpace(line).filter((x) => x != "").length;
-    };
-    return splitByNewLine(data).reduce(wordCounter, 0);
+const countWords = function(data) {
+  const wordCounter = function(count, line) {
+    return count + splitBySpace(line).filter(x => x != "").length;
+  };
+  return splitByNewLine(data).reduce(wordCounter, 0);
 };
 
-const countBytes = function (data) {
-    return data.length;
+const countBytes = function(data) {
+  return data.length;
 };
 
-const getContent = function (fileName, reader) {
-    return reader(fileName, "utf8");
+const getContent = function(fileName, reader) {
+  return reader(fileName, "utf8");
 };
 
-const extractCounts = function (content, options) {
-    let requiredCounts = { "line": countLines, "word": countWords, "byte": countBytes };
-    return options.map((option) => requiredCounts[option](content));
+const extractCounts = function(content, options) {
+  let requiredCounts = { line: countLines, word: countWords, byte: countBytes };
+  return options.map(option => requiredCounts[option](content));
 };
 
-const getFileCount = function (readFileSync, options, fileName) {
-    let content = getContent(fileName, readFileSync);
-    let counts = extractCounts(content, options);
-    return { fileName, counts }
+const getFileCount = function(reader, options, fileName) {
+  let content = getContent(fileName, reader);
+  let counts = extractCounts(content, options);
+  return { fileName, counts };
 };
 
-const wc = function (parsedInputs, readFileSync) {
-    let { options, files } = parsedInputs;
-    let fileCountExtractor = getFileCount.bind("null", readFileSync, options);
-    return files.map(fileCountExtractor);
+const wc = function(parsedInputs, reader) {
+  let { options, files } = parsedInputs;
+  let fileCountExtractor = getFileCount.bind("null", reader, options);
+  return files.map(fileCountExtractor);
 };
 
 module.exports = { wc };
